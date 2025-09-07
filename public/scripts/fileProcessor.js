31cbe0c50e34df47763aa4ac52b0ab0d3c9ce4b6
tropicalalgae <tropicalalgae@gmail.com>
2025-09-07 18:15:34 +0800

    docs: add comment forr a part of function

--- public/scripts/fileProcessor.js
+++ public/scripts/fileProcessor.js
@@ -7,7 +7,13 @@ import {
   removeMdMetadata, 
 } from './mdProcessor.js'
 
-
+/**
+ * 计算文件hash
+ * @param {*} filePath 文件地址
+ * @param {*} algorithm 算法类型
+ * @param {*} length hash长度
+ * @returns 文件hash
+ */
 export function getFileHash(filePath, algorithm = 'sha256', length = 8) {
   const buffer = fs.readFileSync(filePath)
   const hash = crypto.createHash(algorithm)  //（md5/sha1/sha256）
@@ -15,20 +21,39 @@ export function getFileHash(filePath, algorithm = 'sha256', length = 8) {
   return hash.digest("hex").slice(0, length)
 }
 
+/**
+ * 计算文本hash
+ * @param {*} text 文本
+ * @param {*} algorithm 算法类型
+ * @param {*} length hash长度
+ * @returns 文本hash
+ */
 export function getTextHash(text, algorithm = 'sha256', length = 8) {
   const hash = crypto.createHash(algorithm)  //（md5/sha1/sha256）
   hash.update(text, "utf8")
   return hash.digest("hex").slice(0, length)
 }
 
-// 计算新文件的父路径
+/**
+ * 获取文件在新路径下的父目录
+ * @param {*} originFile 原始文件路径
+ * @param {*} originDir 原始文件根路径
+ * @param {*} newDir 新的根路径
+ * @returns 文件在新路径下的父目录
+ */
 export function getNewParentDir(originFile, originDir, newDir) {
   const relPath = path.relative(originDir, originFile)
   const relDir = path.dirname(relPath)
   return path.join(newDir, relDir)
 }
 
-// 基于 原文件/新文本 生成新路径
+/**
+ * 获取原始文件在新目录下带有hash的文件路径
+ * @param {*} originFile 原始文件路径
+ * @param {*} newParentDir 文件在新路径下的父目录
+ * @param {*} newContent 文件内容。若非空，则以该参数计算hash
+ * @returns 带有hash的新文件路径
+ */
 export function getNewHashedPath(originFile, newParentDir, newContent = null) {
   const hash = newContent 
     ? getTextHash(newContent, 'md5')
@@ -40,6 +65,12 @@ export function getNewHashedPath(originFile, newParentDir, newContent = null) {
   return path.join(newParentDir, newName)
 }
 
+/**
+ * 递归获取目录下全部文件
+ * @param {*} dir 目录
+ * @param {*} fileList 已收集的文件列表，用于递归
+ * @returns 文件列表
+ */
 export function getFiles(dir, fileList = []) {
   const entries = fs.readdirSync(dir, { withFileTypes: true })
   for (const entry of entries) {
@@ -54,18 +85,26 @@ export function getFiles(dir, fileList = []) {
   return fileList
 }
 
+/**
+ * 文件排序，将md文件排在最后
+ * @param {*} files 文件路径列表
+ * @returns 排序后的文件路径列表
+ */
 export function sortFiles(files) {
   return files.sort((a, b) => {
     const isAMd = a.toLowerCase().endsWith('.md')
     const isBMd = b.toLowerCase().endsWith('.md')
 
-    if (isAMd && !isBMd) return 1   // a 是 md，b 不是 → a 放后面
-    if (!isAMd && isBMd) return -1  // b 是 md，a 不是 → b 放后面
-    return a.localeCompare(b)       // 否则按字母序
+    if (isAMd && !isBMd) return 1
+    if (!isAMd && isBMd) return -1
+    return a.localeCompare(b)
   })
 }
 
-// 为文件父目录创建文件夹
+/**
+ * 为文件父目录创建文件夹
+ * @param {*} filepath 文件路径
+ */
 export function ensureDirExists(filepath) {
   const dir = path.dirname(filepath)
   fs.mkdirSync(dir, { recursive: true })
@@ -86,9 +125,12 @@ export function clearDirectory(dir) {
 }
 
 /**
- * 获取文件名（去除hash、去除多后缀干扰）
+ * 获取文件名（去除hash、去除连续后缀干扰）
+ * @param {*} filename 含hash与后缀的文件全名。e.g file-8sdf82.md
+ * @param {*} suffixes 后缀词黑名单
+ * @returns 
  */
-export function getFileRealName(filename, excludedExtensions) {
+export function getFileRealName(filename, suffixes) {
   // 去除连续的后缀
   const parts = filename.split('.')
   if (parts.length === 1) return filename
@@ -96,7 +138,7 @@ export function getFileRealName(filename, excludedExtensions) {
   if (parts[i] === 'md') {
     i--
   }
-  while (i > 0 && excludedExtensions.includes(parts[i])) {
+  while (i > 0 && suffixes.includes(parts[i])) {
     i--
   }
   // 去除hash
@@ -106,6 +148,15 @@ export function getFileRealName(filename, excludedExtensions) {
   return fullname.slice(0, lastIndex)
 }
 
+/**
+ * 将路径下的全部markdown文件添加hash，标准化并迁移到新路径下存储
+ * @param {*} config 服务配置
+ * @param {*} sortedFiles 排序后的（md文件在后）文件列表（含md与其他资源文件）
+ * @param {*} publicDir public目录路径
+ * @param {*} originDir 原始文件根路径
+ * @param {*} newDir 新的根路径
+ * @returns 全部markdown的元信息
+ */
 export function movePosts(config, sortedFiles, publicDir, originDir, newDir) {
   const indexData = []
   // 旧数据与新数据的映射表
@@ -117,7 +168,7 @@ export function movePosts(config, sortedFiles, publicDir, originDir, newDir) {
     acc[value.toLowerCase()] = key
     return acc
   }, {})
-  const excludedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg', 'md']
+  const suffixes = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg', 'md']
 
   for (const originFile of sortedFiles) {
     const isMarkdown = originFile.endsWith(".md")
@@ -138,7 +189,7 @@ export function movePosts(config, sortedFiles, publicDir, originDir, newDir) {
 
       const metadata = extractMdMetadata(fixedContent, reverseLabelMap)
       indexData.push({
-        title: getFileRealName(path.basename(newFile), excludedExtensions),
+        title: getFileRealName(path.basename(newFile), suffixes),
         slug: path.basename(newFile),
         dir: sourceDir,
         ...metadata,
@@ -152,6 +203,13 @@ export function movePosts(config, sortedFiles, publicDir, originDir, newDir) {
   return indexData
 }
 
+/**
+ * 将路径下的配置文件计算hash，并迁移到新路径下存储
+ * @param {*} publicDir public目录路径
+ * @param {*} originDir 原始文件根路径
+ * @param {*} newDir 新的根路径
+ * @returns 配置名与新文件的（前端）资源地址的映射
+ */
 export function moveConfig(publicDir, originDir, newDir) {
   const map = {}
   const files = getFiles(originDir)
@@ -166,6 +224,13 @@ export function moveConfig(publicDir, originDir, newDir) {
   return map
 }
 
+/**
+ * 构建markdown的索引文件
+ * @param {*} publicDir public目录路径
+ * @param {*} newDir 新的根路径
+ * @param {*} indexData 全部markdown的元信息
+ * @returns 配置名与新文件的（前端）资源地址的映射
+ */
 export function buildPostIndex(publicDir, newDir, indexData) {
   const indexHash = getTextHash(JSON.stringify(indexData), "md5")
   const indexFile = path.join(newDir, `index-${indexHash}.json`)
--- public/scripts/mdProcessor.js
+++ public/scripts/mdProcessor.js
@@ -1,36 +1,18 @@
 import path from 'path'
 
 
-export function removeMdMetadata(md, matchPrefixes = []) {
-  const lines = md.split('\n')
-  let hasMatchingItem = false
-  let cutIndex = -1
-
-  const prefixRegex = new RegExp(`^[-*+]\\s+(${matchPrefixes.join('|')}):`, 'i')
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].trim()
-
-    if (/^---+$/.test(line)) {
-      cutIndex = i
-      break
-    }
-
-    if (prefixRegex.test(line)) {
-      hasMatchingItem = true
-    }
-  }
-
-  if (cutIndex >= 0 && hasMatchingItem) {
-    return lines.slice(cutIndex + 1).join('\n').trim()
-  }
-
-  return md
-}
-
-export function fixMdAssetsRef(mdContent, originFile, dirPath, fileMap) {
+/**
+ * 修复markdown中对本地资源的引用路径
+ * markdown与引用文件全部迁移至新路径且要在前端渲染，故要替换新的（前端资源）路径
+ * @param {*} mdText markdown文本
+ * @param {*} originFile 原始文件路径
+ * @param {*} sourcePath （前端中）文件的资源父路径
+ * @param {*} fileMap 全部新旧文件的绝对路径映射
+ * @returns 修复后的markdown文本
+ */
+export function fixMdAssetsRef(mdText, originFile, sourcePath, fileMap) {
   // 获取前端访问的资源路径
-  return mdContent.replace(/(!\[.*?\]\()([^\)]+)(\))/g, (match, prefix, imgPath, suffix) => {
+  return mdText.replace(/(!\[.*?\]\()([^\)]+)(\))/g, (match, prefix, imgPath, suffix) => {
     if (/^(https?:)?\/\//.test(imgPath)) return match
 
     // 获取引用文件绝对路径
@@ -46,11 +28,18 @@ export function fixMdAssetsRef(mdContent, originFile, dirPath, fileMap) {
     if (!newAbsPath) {
       return match
     }
-    let relativeToProcessed = path.join(dirPath, path.basename(newAbsPath))
+    let relativeToProcessed = path.join(sourcePath, path.basename(newAbsPath))
     return `${prefix}${relativeToProcessed}${suffix}`
   })
 }
 
+
+/**
+ * 抽取markdown中定义的元信息（标签信息）
+ * @param {*} mdText markdown文本
+ * @param {*} labelMap 标签名的映射
+ * @returns 标签与值的映射
+ */
 export function extractMdMetadata(mdText, labelMap) {
   const lines = mdText.split('\n')
   const result = {}
@@ -88,4 +77,37 @@ export function extractMdMetadata(mdText, labelMap) {
   if (!result.tags) result.tags = []
 
   return result
+}
+
+/**
+ * 移除markdown中的定义的元信息（标签信息）
+ * @param {*} mdText markdown文本
+ * @param {*} matchPrefixes 标签名列表
+ * @returns 移除元信息后的markdown
+ */
+export function removeMdMetadata(mdText, matchPrefixes = []) {
+  const lines = mdText.split('\n')
+  let hasMatchingItem = false
+  let cutIndex = -1
+
+  const prefixRegex = new RegExp(`^[-*+]\\s+(${matchPrefixes.join('|')}):`, 'i')
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim()
+
+    if (/^---+$/.test(line)) {
+      cutIndex = i
+      break
+    }
+
+    if (prefixRegex.test(line)) {
+      hasMatchingItem = true
+    }
+  }
+
+  if (cutIndex >= 0 && hasMatchingItem) {
+    return lines.slice(cutIndex + 1).join('\n').trim()
+  }
+
+  return mdText
 }
