c9f2cc71ce4d722dd599dbf85dc8e8b6b5581dc2
tropicalalgae <tropicalalgae@gmail.com>
2025-09-13 17:03:59 +0800

    chore: update release message

--- release.config.cjs
+++ release.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     ["@semantic-release/npm", { npmPublish: false }],
     ["@semantic-release/git", { 
     assets: ["package.json", "CHANGELOG.md"], 
-    message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}" 
+    message: "${nextRelease.version} Release!\n\n${nextRelease.notes}" 
     }],
     "@semantic-release/github"
   ]
