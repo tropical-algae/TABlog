7c60046caac4cc0118466655457ce635ed07668f
tropicalalgae <tropicalalgae@gmail.com>
2025-09-13 12:38:04 +0800

    refactor: optimize css

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,8 @@ npm run build
 chmod +x ./dist/scripts/run.sh
 echo "✅ Successfully built web, get ready to build Docker Image!"
 
+rm -rf dist/config_processed/* dist/markdowns/* dist/markdowns_processed/*
+echo "✅ Successfully cleared cache"
+
 docker build -t $IMAGE_NAME:$IMAGE_VERSION .
 echo "✅ Successfully built build Docker Image!"
