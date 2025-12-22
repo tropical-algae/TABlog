16281031e383a682d120098e10e61062c702e84e
tropicalalgae <tropicalalgae@gmail.com>
2025-12-22 17:38:39 +0800

    feat: optimized the style of two layouts

--- src/layouts/IntroOnlyLayout.vue
+++ src/layouts/IntroOnlyLayout.vue
@@ -3,7 +3,7 @@
     <div class="px-md-4 px-3 pt-4 m-0 d-flex flex-column min-full-height">
       <div class="flex-grow-1 d-flex flex-column router-elem-fade">
         <RouterView v-slot="{ Component }">
-          <component :is="Component" :key="$route.fullPath" />
+          <component :is="Component" :key="$route.fullPath" class="flex-grow-1"/>
         </RouterView>
       </div>
       <FooterBar/>
